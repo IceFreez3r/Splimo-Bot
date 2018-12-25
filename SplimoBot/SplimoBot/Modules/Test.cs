523922347331fad13f1d15748538876576fa9784
IceFreez3r <tom_lukas@bueroblau.de>
2018-12-25 19:43:17 +0100

    CommandHandler und Test gefüllt

--- SplimoBot/SplimoBot/Modules/Test.cs
+++ SplimoBot/SplimoBot/Modules/Test.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
 
 namespace SplimoBot.Modules
 {
-    class Test
+    public class Test : ModuleBase<SocketCommandContext>
     {
+        [Command("test")]
+        public async Task one(IRole role)
+        {
+            await Context.Channel.SendMessageAsync("Kilian ist " + role.ToString());
+        }
+        [Command("lol")]
+        public async Task two(IGuildUser user)
+        {
+            await Context.Channel.SendMessageAsync("Kilian ist " + user.ToString());
+        }
+        [Command("rofl")]
+        public async Task three(string repeat)
+        {
+            await Context.Channel.SendMessageAsync("Kilian ist " + repeat.ToString());
+        }
     }
 }
