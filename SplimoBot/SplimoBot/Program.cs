f6998958e07509226ea2fd7f96393620abd6b20e
IceFreez3r <tom_lukas@bueroblau.de>
2018-12-25 19:33:59 +0100

    Initial Commit

---
+++ SplimoBot/SplimoBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SplimoBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
