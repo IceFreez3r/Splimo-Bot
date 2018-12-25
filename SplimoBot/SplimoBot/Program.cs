3900bac4cde188ab3390618bd89eee74b603cd16
IceFreez3r <tom_lukas@bueroblau.de>
2018-12-25 19:38:04 +0100

    Program.cs hinzugefügt

--- SplimoBot/SplimoBot/Program.cs
+++ SplimoBot/SplimoBot/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
 
 namespace SplimoBot
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            await _client.LoginAsync(TokenType.Bot, "NTIzODIzNTY2MTY5NTcxMzM4.DwOOUw.vCMyfbTOvyAqjlc3LTFIxhQy6nY");
+            await _client.StartAsync();
+            _handler = new CommandHandler(_client);
+            await Task.Delay(-1);
         }
     }
 }
